cedc1668358f3aaf8ce8711aa68325d339e60e7c
DorogiB <dorogi.benjamin@lightport.hu>
2025-04-10 12:28:19 +0200

    remove checkmark from chip

--- lib/chip_display/multi_select_chip_display.dart
+++ lib/chip_display/multi_select_chip_display.dart
@@ -130,6 +130,7 @@ class MultiSelectChipDisplay<V> extends StatelessWidget {
     return Container(
       padding: const EdgeInsets.all(2.0),
       child: ChoiceChip(
+        showCheckmark: false,
         shape: shape as OutlinedBorder?,
         avatar: icon != null
             ? Icon(
